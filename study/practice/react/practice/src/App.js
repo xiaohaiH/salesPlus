fb0ae67811e6f0cbb3e7ec3066365707aa1182c4
Betty <bettyzhang@salesplus.com.cn>
2017-08-15 13:47:19 +0800

    react与vue环境搭建

--- study/practice/react/practice/src/App.js
+++ study/practice/react/practice/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // import Home from './View/Home/Home';
 // import UrlArguments from './View/UrlArguments/UrlArguments'
 // import Redirect from './View/Redirect/Redirect'
-import Test from './test'
+import Test from './test';
 
 class App extends Component {
   render() {

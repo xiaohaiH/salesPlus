2aac69d1e0ef600e865ab863cdde0e04cdbf6ceb
Betty <bettyzhang@salesplus.com.cn>
2017-08-15 14:08:21 +0800

    react环境搭建2017-8-15

--- study/practice/react/practice/src/App.js
+++ study/practice/react/practice/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-
-// import Home from './View/Home/Home';
-// import UrlArguments from './View/UrlArguments/UrlArguments'
-// import Redirect from './View/Redirect/Redirect'
-import Test from './test';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        {/* <Home /> */}
-        {/* <UrlArguments /> */}
-        {/* <Redirect /> */}
-        <Test />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }

73cdf7d95589adef6ba0486a276eeb6f70d26740
Betty <bettyzhang@salesplus.com.cn>
2017-08-25 13:37:13 +0800

    2017-8-25react搭配dva环境

--- study/practice/react/practice/src/App.js
+++ study/practice/react/practice/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+// import ReactDOM from "react-dom";
 // import Home from './View/Home/Home';
 // import UrlArguments from './View/UrlArguments/UrlArguments';
 // import Redirect from './View/Redirect/Redirect';
+
 import Redirect from './View/Redirect/Redirect';
 import Indexa from './View/Redirect/index';
 import Test from './View/Redirect/test';
@@ -21,7 +23,6 @@ import RecursionPathParctice from './View/RecursionPath/parcticeAndFormSubmit';
 
 /* 侧边栏 */
 import SideBar from './View/SideBar/sidebar';
-
 
 class App extends Component {
   render() {
@@ -72,12 +73,50 @@ class App extends Component {
             <RecursionPathParctice />
           </div>
         </div>
-        <div style={{padding: '40px 50px',background: 'dark'}}>
+        <div style={{padding: '40px 50px',background: 'dark',overflow: 'hidden'}}>
           <SideBar />
         </div>
       </div>
     );
   }
-}
+};
+
+
+
+//实现css动画
+// class App extends Component {
+  
+//   render() {
+//     const items = this.state.items.map((item, i) => (
+//       <div key={item} onClick={() => this.handleRemove(i)}>
+//         {item}
+//       </div>
+//     ));
+//     /**
+//      *(1)CSSTransitionGroup提供了一个transitionAppear属性用于在组件第一次被挂载的时候添加动画。
+//     *默认情况下，在组件第一次被挂载的时候我们的transitionAppear被设置为false。
+//     *如果要为首次挂载添加动画你要使用该属性。它会自动添加 example-appear和example-appear-active.
+//     *(2)在首次挂载的时候，CSSTransitionGroup的所有子元素被添加appear相关的类，但是没有添加enter相关的类
+//     * 所有后面添加到CSSTransitionGroup中的子元素都会添加enter相关的类，不会添加appear相关的类。
+//     *(3)transitionAppear是在0.13后添加的，为了向后兼容默认设置为false。但是默认的transitionEnter和
+//     * transitionLeave被设置为true，所以你必须指定transitionEnterTimeout和transitionLeaveTimeout。
+//     * 如果你不需要enter和leave相关的动画，请设置transitionEnter={false}或者transitionLeave={false}
+//     */
+//     return (
+//       <Router>
+//       <div>
+//         <style dangerouslySetInnerHTML={{ __html: this.getCssClss() }} />
+//         <button onClick={this.handleAdd}>Add Item</button>
+//         <CSSTransitionGroup transitionName="example" transitionEnterTimeout={500} transitionLeaveTimeout={300} transitionAppear={true} transitionAppearTimeout={500}>
+//           {items}
+//         </CSSTransitionGroup>
+//         <Link to=''>123</Link>
+//         <Route path='/' render={() => <h3>123</h3>} />
+//       </div>
+//       </Router>
+//     );
+//   }
+// };
+
 
 export default App;

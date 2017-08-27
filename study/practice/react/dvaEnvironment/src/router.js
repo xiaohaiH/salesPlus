641a8fb5eee9471d15d3687d88771b9bc9bb0f03
xiaohaiH <773884562@qq.com>
2017-08-27 21:12:38 +0800

    dva+ant环境搭建

--- study/practice/react/dvaEnvironment/src/router.js
+++ study/practice/react/dvaEnvironment/src/router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+// import IndexPage from './routes/IndexPage';
 import products from './routes/products';
 
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={IndexPage} />
       <Route path="/products" component={products} />
+      <Route path="/" component={IndexPage} />
     </Router>
   );
 }

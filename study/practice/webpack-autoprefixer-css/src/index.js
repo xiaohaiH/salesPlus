83820ad662eef8fd7b8aae02530df076b02c9038
Betty <bettyzhang@salesplus.com.cn>
2017-09-07 18:56:04 +0800

    sass(scss)&less环境搭建完成

--- study/practice/webpack-autoprefixer-css/src/index.js
+++ study/practice/webpack-autoprefixer-css/src/index.js
@@ -1,1 +1,26 @@
-require('index.css')
+require('./less/main.less');
+require('./scss/main.scss');
+require('./style/main.css');
+const test = ({a = 5,b = 10}={}) => {
+  console.log(a,b)
+};
+test();
+class A {
+  handle({name = 'xiaohai'}){
+    this.name = name;
+  }
+}
+const a = new A();
+a.handle({});
+console.log(a);
+
+class B extends A{
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    alert(123);
+  }
+};
+new B().handleClick();
--- study/practice/webpack-autoprefixer-css/webpack.config.js
+++ study/practice/webpack-autoprefixer-css/webpack.config.js
@@ -1,39 +1,77 @@
 const path = require('path');
+const webpack = require('webpack');
 const Clean = require('clean-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 const Ex = require('extract-text-webpack-plugin');
+const ImgPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
-  entry: ['./src/index.css'],
+  entry: './src/index.js',
   output: {
-    filename: 'remove.css',
-    path: path.resolve(__dirname,'dist')
+    filename: 'bundle-[chunkhash:8].js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
-        test: /\.(css|sass|less)$/,
+        test: /\.html$/,
+        use: [
+          {loader: 'html-loader'}
+        ]
+      },
+      {
+        test: /\.js(x)?$/,
+        use: [
+          {loader: 'babel-loader'}
+        ]
+      },
+      {
+        test: /\.(css|less)$/,
         use: Ex.extract({
           fallback: 'style-loader',
           use: [
-            {
-              loader: 'css-loader'
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [require('autoprefixer')]
-              }
-            }
+            {loader: 'css-loader'},
+            {loader: 'postcss-loader'},
+            {loader: 'less-loader'}
           ]
         })
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: Ex.extract({
+          fallback: 'style-loader',
+          use: [
+            {loader: 'css-loader'},
+            {loader: 'postcss-loader'},
+            {loader: 'sass-loader'}
+          ]
+        })
+      },
+      {
+        test: /\.(jp(e)?g|png|bmp|gif)$/,
+        use: [
+          {loader: 'file-loader?limit=5000&name=assets/[name]-[hash:8].[ext]'}
+        ]
       }
     ]
   },
   plugins: [
+    new HtmlPlugin({
+      template: './src/main.html',
+      innest: 'body',
+      filename: 'main-[chunkhash:8].html'
+    }),
     new Clean(['dist'],{
       root: '',
+      verbose: true,
       dry: false
     }),
-    new Ex('[name].css')
+    new webpack.optimize.UglifyJsPlugin(),
+    new ImgPlugin({
+      pngquant: {
+        quality: '95-100'
+      }
+    }),
+    new Ex('main.css')
   ]
 }
